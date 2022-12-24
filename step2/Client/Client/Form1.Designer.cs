fe842a188d7b0442e546d64af9b761190844a19f
Ahmet Mihca Aydin <ahmetmihcaaydin@gmail.com>
2022-12-24 14:49:12 +0300

    step 2

---
+++ step2/Client/Client/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_ip = new System.Windows.Forms.Label();
+            this.label_port = new System.Windows.Forms.Label();
+            this.label_name = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.richTextBox_logs = new System.Windows.Forms.RichTextBox();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.button_send = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_ip
+            // 
+            this.label_ip.AutoSize = true;
+            this.label_ip.Location = new System.Drawing.Point(76, 69);
+            this.label_ip.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_ip.Name = "label_ip";
+            this.label_ip.Size = new System.Drawing.Size(22, 16);
+            this.label_ip.TabIndex = 0;
+            this.label_ip.Text = "IP:";
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(63, 106);
+            this.label_port.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(34, 16);
+            this.label_port.TabIndex = 1;
+            this.label_port.Text = "Port:";
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Location = new System.Drawing.Point(52, 148);
+            this.label_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(47, 16);
+            this.label_name.TabIndex = 2;
+            this.label_name.Text = "Name:";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(103, 66);
+            this.textBox_ip.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(224, 22);
+            this.textBox_ip.TabIndex = 3;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(103, 106);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(224, 22);
+            this.textBox_port.TabIndex = 4;
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(103, 145);
+            this.textBox_name.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(224, 22);
+            this.textBox_name.TabIndex = 5;
+            // 
+            // richTextBox_logs
+            // 
+            this.richTextBox_logs.Location = new System.Drawing.Point(385, 65);
+            this.richTextBox_logs.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox_logs.Name = "richTextBox_logs";
+            this.richTextBox_logs.Size = new System.Drawing.Size(448, 258);
+            this.richTextBox_logs.TabIndex = 6;
+            this.richTextBox_logs.Text = "";
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Enabled = false;
+            this.textBox_message.Location = new System.Drawing.Point(385, 367);
+            this.textBox_message.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(337, 22);
+            this.textBox_message.TabIndex = 7;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(103, 206);
+            this.button_connect.Margin = new System.Windows.Forms.Padding(4);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(100, 39);
+            this.button_connect.TabIndex = 8;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button_disconnect.Enabled = false;
+            this.button_disconnect.Location = new System.Drawing.Point(228, 206);
+            this.button_disconnect.Margin = new System.Windows.Forms.Padding(4);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(100, 39);
+            this.button_disconnect.TabIndex = 9;
+            this.button_disconnect.Text = "Disconnect";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            this.button_disconnect.Click += new System.EventHandler(this.button_disconnect_Click);
+            // 
+            // button_send
+            // 
+            this.button_send.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(748, 367);
+            this.button_send.Margin = new System.Windows.Forms.Padding(4);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(85, 25);
+            this.button_send.TabIndex = 10;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Logs:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(385, 345);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Message:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 480);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.button_disconnect);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.textBox_message);
+            this.Controls.Add(this.richTextBox_logs);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.label_port);
+            this.Controls.Add(this.label_ip);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_ip;
+        private System.Windows.Forms.Label label_port;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.RichTextBox richTextBox_logs;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
